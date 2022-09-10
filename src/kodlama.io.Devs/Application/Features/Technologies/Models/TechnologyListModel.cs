d6eb0a39b75e80128042d19776be70da355b5cdd
ahmethamzaustun <a.hamzaa@icloud.com>
2022-09-11 01:59:32 +0300

    ByDynamicQuery

--- src/kodlama.io.Devs/Application/Features/Technologies/Models/TechnologyListModel.cs
+++ src/kodlama.io.Devs/Application/Features/Technologies/Models/TechnologyListModel.cs
@@ -1,9 +1,9 @@
-using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Features.Technologies.Dtos;
 using Core.Persistence.Paging;
 
 namespace Application.Features.Technologies.Models;
 
 public class TechnologyListModel:BasePageableModel
 {
-    public IList<ProgrammingLanguageListDto> Items { get; set; }
+    public IList<TechnologyListDto>? Items { get; set; }
 }
